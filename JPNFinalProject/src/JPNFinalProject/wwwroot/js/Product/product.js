800ab4c2e93f481fca435ad86609b2c1c581d484
phvorret <phvorret@LAPTOP-M7P4SJK3>
2016-11-08 13:17:25 +0100

    Lidt flere changes

---
+++ JPNFinalProject/src/JPNFinalProject/wwwroot/js/Product/product.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#adjust').click(function () {
+        if ($('.extrasearch').css('display') == 'none')
+        {
+            $('.extrasearch').css('display', 'unset');
+        }
+        if ($('.extrasearch').css('display') == 'unset')
+        {
+            $('.extrasearch').css('display', 'none');
+        }
+    });
+});
