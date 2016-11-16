5da218cf70f87b22ef9b67f3b233a27388cf74e4
NicolajPedersen <nicolajgpedersen@gmail.com>
2016-11-16 10:33:15 +0100

    Session ordnet

--- JPNFinalProject/src/JPNFinalProject/Services/SessionContainer.cs
+++ JPNFinalProject/src/JPNFinalProject/Services/SessionContainer.cs
@@ -1,31 +1,42 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JPNFinalProject.Services
 {
     public class SessionContainer : Controller
     {
-        public void AddToSession(string key, string value) {
-            HttpContext.Session.SetString(key, value);
-        }
-
-        public string GetSessionString(string key) {
-            return HttpContext.Session.GetString(key);
-        }
-
-        public List<int> GetBasket(string key) {
-            List<int> ids = new List<int>();
-            foreach (var i in HttpContext.Session.Keys) {
-                if (i == key) {
-                    ids.Add(Convert.ToInt32(i));
-                }
+        public void AddToSession(HttpContext context, string key, int value) {
+            try {
+                var item = HttpContext.Session.GetString(key);
+                List<int> basket1 = JsonConvert.DeserializeObject<List<int>>(item);
+                basket1.Add(value);
+                context.Session.SetString(key, JsonConvert.SerializeObject(basket1));
             }
-            return ids;
+            catch {
+                List<int> basket = new List<int>();
+                basket.Add(value);
+                context.Session.SetString(key, JsonConvert.SerializeObject(basket));
+            }
+        }
+
+        public string GetSessionString(HttpContext context, string key) {
+            return context.Session.GetString(key);
+        }
+
+        public List<int> GetBasket(HttpContext context, string key) {
+            try {
+                var item = context.Session.GetString(key);
+                List<int> productIds = JsonConvert.DeserializeObject<List<int>>(item);
+                return productIds;
+            }
+            catch {
+                return null;
+            }
         }
     }
 }
