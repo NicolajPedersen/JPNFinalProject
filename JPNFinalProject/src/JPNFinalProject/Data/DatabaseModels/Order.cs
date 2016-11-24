38e165ee02cc6facf5c63f46d4f09109a1b3f152
Jens Skjold <jesk@hesehus.dk>
2016-11-24 09:43:49 +0100

    new database

--- JPNFinalProject/src/JPNFinalProject/Data/DatabaseModels/Order.cs
+++ JPNFinalProject/src/JPNFinalProject/Data/DatabaseModels/Order.cs
@@ -5,11 +5,19 @@ namespace JPNFinalProject.Data.DatabaseModels
 {
     public partial class Order
     {
+        public Order()
+        {
+            BusinessOrder = new HashSet<BusinessOrder>();
+            OrderProduct = new HashSet<OrderProduct>();
+        }
+
         public int OrderId { get; set; }
         public int PersonId { get; set; }
         public decimal TotalPrice { get; set; }
         public DateTimeOffset DeliveryTime { get; set; }
 
+        public virtual ICollection<BusinessOrder> BusinessOrder { get; set; }
+        public virtual ICollection<OrderProduct> OrderProduct { get; set; }
         public virtual Person Person { get; set; }
     }
 }
