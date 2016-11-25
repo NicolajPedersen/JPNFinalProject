94285c6032d7498484d88d5d21590958d8422b5d
Jens Skjold <jesk@hesehus.dk>
2016-11-25 10:33:28 +0100

    migrations

---
+++ JPNFinalProject/src/JPNFinalProject/Migrations/20161125091726_Produccategoryupdate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JPNFinalProject.Migrations
+{
+    public partial class Produccategoryupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ JPNFinalProject/src/JPNFinalProject/Migrations/20161125092153_testmig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JPNFinalProject.Migrations
+{
+    public partial class testmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestString",
+                table: "ProductCategory",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestString",
+                table: "ProductCategory");
+        }
+    }
+}
