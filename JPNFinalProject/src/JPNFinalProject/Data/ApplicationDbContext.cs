c366d7311eebe07458e283d4e35eea2367bfc1c0
Jens Skjold <jesk@hesehus.dk>
2016-11-25 09:49:26 +0100

    dbcontex!

--- JPNFinalProject/src/JPNFinalProject/Data/ApplicationDbContext.cs
+++ JPNFinalProject/src/JPNFinalProject/Data/ApplicationDbContext.cs
@@ -9,15 +9,24 @@ using JPNFinalProject.Models;
 namespace JPNFinalProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
-        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        //    : base(options) {
-        //}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
-        //protected override void OnModelCreating(ModelBuilder builder) {
-        //    base.OnModelCreating(builder);
-        //    // Customize the ASP.NET Identity model and override the defaults if needed.
-        //    // For example, you can rename the ASP.NET Identity table names and more.
-        //    // Add your customizations after calling base.OnModelCreating(builder);
-        //}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Data Source=dev-db02\sql2016;Initial Catalog=JPNFinalProject;Persist Security Info=True;User ID=JPNFinalProject;Password=JPNFinalProject2016;");
+        }
     }
 }
+
