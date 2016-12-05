560aab731e65551a5f040ce229e56d267cbddc07
NicolajPedersen <nicolajgpedersen@gmail.com>
2016-12-05 10:21:17 +0100

    console fjernet

--- JPNFinalProject/src/JPNFinalProject/wwwroot/js/Order/paymentDone.js
+++ JPNFinalProject/src/JPNFinalProject/wwwroot/js/Order/paymentDone.js
@@ -2,11 +2,8 @@
     console.log($("#productInfo > .row"));
     $("#productInfo > .row").each(function () {
         var productPrice = $(this).find("#productPrice").html();
-        console.log(productPrice);
         var quantity = $(this).find("#amount").html();
-        console.log(quantity);
         var price = productPrice * quantity;
-        console.log(price);
         $(this).find("#totalPrice").html(price);
     });
 });
