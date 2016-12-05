fce4acb42a85056379a15cbededbb61d8f7b75dc
NicolajPedersen <nicolajgpedersen@gmail.com>
2016-12-05 10:20:25 +0100

    PaymentDone ændring

--- JPNFinalProject/src/JPNFinalProject/wwwroot/js/Order/paymentDone.js
+++ JPNFinalProject/src/JPNFinalProject/wwwroot/js/Order/paymentDone.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    console.log($("#productInfo > .row"));
+    $("#productInfo > .row").each(function () {
+        var productPrice = $(this).find("#productPrice").html();
+        console.log(productPrice);
+        var quantity = $(this).find("#amount").html();
+        console.log(quantity);
+        var price = productPrice * quantity;
+        console.log(price);
+        $(this).find("#totalPrice").html(price);
+    });
+});
