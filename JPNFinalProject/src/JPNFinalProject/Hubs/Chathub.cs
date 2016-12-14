82782e64a1813311a6127ed1d133eb82344d9f33
jens <jens@DESKTOP-UUK73D3>
2016-12-14 20:53:09 +0100

    basic broadcast chat

---
+++ JPNFinalProject/src/JPNFinalProject/Hubs/Chathub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+
+namespace JPNFinalProject.Hubs
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            string name = Context.ConnectionId;
+            Clients.All.onMessageReceived(DateTime.Now.ToString(), name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            string name = Context.ConnectionId;
+            Groups.Add(Context.ConnectionId, name);
+            return base.OnConnected();
+        }
+    }
+}
