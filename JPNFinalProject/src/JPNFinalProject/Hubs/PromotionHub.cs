f40d06519d46e4ab1a61009c040844e906ae7c84
jens <jens@DESKTOP-UUK73D3>
2017-01-04 18:56:41 +0100

    quickie

---
+++ JPNFinalProject/src/JPNFinalProject/Hubs/PromotionHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+
+namespace JPNFinalProject.Hubs
+{
+    [HubName("promotion")]
+    public class PromotionHub : Hub
+    {
+        public void SendPromotion(string message)
+        {
+            string querytype = Context.QueryString["transport"];
+            string name = Context.ConnectionId;
+            Clients.Others.onMessageReceived(message);
+        }
+
+        public override Task OnConnected()
+        {
+            string name = Context.ConnectionId;
+            Groups.Add(Context.ConnectionId, name);
+            return base.OnConnected();
+        }
+    }
+}
